3540afa1507fa2b8571ff99903a211ba032579c6
Veltys <veltys@veltys.es>
2019-06-14 23:12:24 +0200

    ADD: Mínimo paramétrico

--- grafico_temperaturas.sh
+++ grafico_temperaturas.sh
@@ -4,7 +4,7 @@
 # Description   : Muestra un gráfico ASCII de las temperaturas de un log
 # Author        : Veltys
 # Date          : 14-06-2019
-# Version       : 0.5.1
+# Version       : 0.6.0
 # Usage         : sudo bash grafico_temperaturas.sh | ./grafico_temperaturas.sh
 # Notes         :
 
@@ -18,9 +18,14 @@ redondear() {
   printf "%.${2}f" "${1}"
 }
 
+
 ## Ejecución principal
+if [ "$#" -eq 2 ]; then
+	minimo=$2
+fi
+
 if [ "$#" -lt 1 ]; then
-	echo "Uso: $0 archivo_log"
+	echo "Uso: $0 archivo_log [minimo]"
 else
 	horas=($(cat $1 | grep -v '^Informe\ diario\ de\ ' | grep -v '^$' | sed -r -e 's/[[:print:]]*([0-9]{2}\:[0-9]{2})\:[0-9]{2}\ CEST[[:print:]]*/\1/'))
 
