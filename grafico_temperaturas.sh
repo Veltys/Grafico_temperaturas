d909f3d5e3fbce97cbbfa0f594cc27758dd91fd4
Veltys <veltys@veltys.es>
2019-06-14 21:05:07 +0200

    ADD: Esqueleto del script

---
+++ grafico_temperaturas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Title         : grafico_temperaturas.sh
+# Description   : Muestra un gráfico ASCII de las temperaturas de un log
+# Author        : Veltys
+# Date          : 14-06-2019
+# Version       : 0.0.1
+# Usage         : sudo bash grafico_temperaturas.sh | ./grafico_temperaturas.sh
+# Notes         :
+
+
+## Variables
+
+
+## Funciones
+
+
+## Ejecución principal
+
