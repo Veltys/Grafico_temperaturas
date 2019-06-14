ba49e4adbc4ad821087290d7e12b272330197f7d
Veltys <veltys@veltys.es>
2019-06-15 00:14:05 +0200

    ADD: Cálculo del máximo

--- grafico_temperaturas.sh
+++ grafico_temperaturas.sh
@@ -4,7 +4,7 @@
 # Description   : Muestra un gráfico ASCII de las temperaturas de un log
 # Author        : Veltys
 # Date          : 14-06-2019
-# Version       : 0.2.2
+# Version       : 0.2.3
 # Usage         : sudo bash grafico_temperaturas.sh | ./grafico_temperaturas.sh
 # Notes         :
 
@@ -22,13 +22,20 @@ if [ "$#" -lt 1 ]; then
 	echo "Uso: $0 archivo_log"
 else
 	horas=($(cat $1 | grep -v '^Informe\ diario\ de\ ' | grep -v '^$' | sed -r -e 's/[[:print:]]*([0-9]{2}\:[0-9]{2})\:[0-9]{2}\ CEST[[:print:]]*/\1/'))
+
 	temperaturas=($(cat $1 | grep -v '^Informe\ diario\ de\ ' | grep -v '^$' | sed -r -e 's/[[:print:]]*temp\=([0-9]{2})\.([0-9])[[:print:]]*/\1,\2/'))
+
+	maximo=0
 
 	for (( i=0; i<${#horas[@]}; i++ )); do
 		temperaturas[$i]=$(redondear ${temperaturas[$i]} 0)
+
+		if [ maximo<${temperaturas[$i]} ]; then
+			maximo=${temperaturas[$i]}
+		fi
 	done
 
-	for (( i=100; i>=-1; i-- )); do
+	for (( i=$maximo; i>=-1; i-- )); do
 		if [ i>-1 ]; then
 			echo "$i"
 		else
